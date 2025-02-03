6e9229be14e8f42d6dd1614e17066e07d8b5e312
LOKA899 <neel.puneeth@gmail.com>
2025-02-03 21:34:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -222,6 +222,6 @@ def index():
 
 # Press the green button in the gutter to run the script.
 if __name__ == "__main__":
-    flask_app.run(host="0.0.0.0", port=5000, debug=True)
+    flask_app.run(host="0.0.0.0", port=5000, debug=False)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
